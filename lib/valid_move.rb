44565668db8d12978f0ccb7fefd1fae64ceeb97f
Julianna Tetreault <juliannatetreault@icloud.com>
2018-08-30 09:17:04 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken?.between?(1, 9)
+  if index.between?(1, 9)
     return true
   else position_taken?
    return false || nil
