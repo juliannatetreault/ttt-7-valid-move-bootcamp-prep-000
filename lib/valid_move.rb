dfc3af73998bbbe37dc04138e2aaa36f8bf370de
Julianna Tetreault <juliannatetreault@icloud.com>
2018-08-30 09:08:34 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,8 +7,9 @@ end
 def position_taken?(board, index)
   if board[index] == " " || board[index] == ""
     return false
-    elsif board[index] == nil
+  elsif board[index] == nil
     return false
   else board[index] == "X" || board[index] == "O"
     return true
+end
 end
